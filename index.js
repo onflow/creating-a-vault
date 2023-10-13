9488cf7cfafe497219b8e15f5c945bb5bb9de4ee
lealobanov <lea.lobanov@icloud.com>
2023-10-13 20:05:28 +0900

    Removing description

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
 export const creatingAVault = {
   slug: recipe,
   title: "Creating a Vault",
-  description: "",
   createdAt: Date(2022, 3, 1),
   author: "Flow Blockchain",
   playgroundLink:
