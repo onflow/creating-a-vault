71d4d2d66d81b7bd4c411911cbbcec83a2fc71c0
lealobanov <lea.lobanov@icloud.com>
2023-10-16 22:59:29 +0900

    Add README template and license

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ export const creatingAVault = {
     "https://play.onflow.org/ef2fe054-148b-4c75-94f1-95bd33b6ce00?type=tx&id=1629c020-d908-475e-87db-c81d1c496964",
   excerpt:
     "This explains the function that would be used for you to create a new vault in others accounts.",
-
   smartContractCode: contractPath,
   smartContractExplanation: smartContractExplanationPath,
   transactionCode: transactionPath,
